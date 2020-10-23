704feb341ec0e9cd2042f223166ddb34128e0e9a
xuepl <xuepl@guoyasoft.com>
2020-10-23 16:32:05 +0800

    ui自动化框架封板1

--- run.py
+++ run.py
@@ -9,13 +9,13 @@ if __name__ == '__main__':
     # 修改成要执行的测试用例                                                               
     test_case = './test_case/demo'
                                                                                            
-    xml_report_path = './reports/xml/'
+    xml_report_path = './allure-results/'
     html_report_path = './reports/html/'
                                                                                            
     pytest.main(['-s', '-q', '--alluredir',                                                
                  xml_report_path, test_case])                                              
-    cmd1 = 'allure generate %s -o %s --clean' % (xml_report_path, html_report_path)
-                                                                                           
-    shell_tool.invoke(cmd1)
+    # cmd1 = 'allure generate %s -o %s --clean' % (xml_report_path, html_report_path)
+    #
+    # shell_tool.invoke(cmd1)
 
 
